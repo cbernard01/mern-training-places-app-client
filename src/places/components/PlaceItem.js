f503491e2053bb45d2c0472098dd86ea87db497b
Clifford Bernard <clifford@verticalfront.com>
2020-03-24 17:36:42 -0500

    Delete Modal Commit:
    Added the delete modal feature.

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -8,8 +8,12 @@ import "./PlaceItem.css";
 
 const PlaceItem = props => {
   const [showMap, setShowMap] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
   const openMapHandler = () => setShowMap(true);
   const closeMapHandler = () => setShowMap(false);
+  const openDeleteWarningHandler = () => setShowConfirmModal(true);
+  const closeDeleteHandler = () => setShowConfirmModal(false);
+  const confirmDeleteHandler = () => setShowConfirmModal(false);
 
   return (
     <React.Fragment>
@@ -25,6 +29,19 @@ const PlaceItem = props => {
           <Map center={props.location} zoom={16}/>
         </div>
       </Modal>
+      <Modal
+        show={showConfirmModal}
+        header={"Are you sure?"}
+        footerClass={"place-item__modal-actions"}
+        footer={
+          <React.Fragment>
+            <Button inverse onClick={closeDeleteHandler}>CANCEL</Button>
+            <Button danger onClick={confirmDeleteHandler}>DELETE</Button>
+          </React.Fragment>
+        }
+      >
+        <p>Do you want to proceed and delete this place? (Cannot be undone.)</p>
+      </Modal>
       <li className={"place-item"}>
         <Card className={"place-item__content"}>
           <div className={"place-item__image"}>
@@ -38,7 +55,7 @@ const PlaceItem = props => {
           <div className={"place-item__actions"}>
             <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
             <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button danger onClick={openDeleteWarningHandler}>DELETE</Button>
           </div>
         </Card>
       </li>
--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -2,19 +2,21 @@ import React from "react";
 
 import UsersList from "../components/UsersList";
 
-const Users = () => {
-  const USERS = [{
+export const USERS = [
+  {
     id: "u1",
     name: "Clifford Bernard",
     image: "https://ak6.picdn.net/shutterstock/videos/9925526/thumb/1.jpg",
     places: 4
-  },{
+  }, {
     id: "u2",
     name: "Megan Bernard",
     image: "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     places: 2
-  }];
+  }
+];
 
+const Users = () => {
   return (
     <UsersList items={USERS}/>
   );
