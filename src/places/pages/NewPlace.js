0894323ea1522ff940d57fe260b8537ff4e7968c
Clifford Bernard <clifford@verticalfront.com>
2020-04-07 13:55:34 -0500

    Create Place Creator Commit:
    Removed the requirement of a creator id being sent from the frontend to the backend.  This change removes the creator id from being passed between the two frameworks.  The backend will evaluate the JWT token to use the correct User Id for the creator field.

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -29,7 +29,6 @@ const NewPlace = props => {
       formData.append("description", formState.inputs.description.value);
       formData.append("address", formState.inputs.address.value);
       formData.append("image", formState.inputs.image.value);
-      formData.append("creator", auth.userId);
 
       await sendRequest("http://localhost:5000/api/places",
         "POST",
